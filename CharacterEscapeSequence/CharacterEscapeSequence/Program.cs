c0fbc48c7aed00b7775f024458222dd5697879ef
CARICOM12 <0190a@ukr.net>
2020-05-03 15:16:38 +0300

    Add files via upload

---
+++ CharacterEscapeSequence/CharacterEscapeSequence/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CharacterEscapeSequence
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(\a + \b + \f + \n + \r + \t + \v + \0 + \' + \" + \\);
+        }
+    }
+}
